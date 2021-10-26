bec8cbf21f53db158da176d824a6193b6a41b55e
Sony <Sony@Sony-PC>
2021-10-26 18:00:34 +0530

    hey hii mayur m commiting my first code

---
+++ mayur/src/mayurgudboy/Smartboy.java
@@ -0,0 +1,11 @@
+package mayurgudboy;
+
+public class Smartboy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey hii mayur");
+
+	}
+
+}
